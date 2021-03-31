6beb07c41eacff071fe959443d22f8caf1a23fa2
Alex Huppert <ahuppert@interos.ai>
2021-03-31 13:37:10 -0400

    Track custom-file.el

---
+++ .emacs.d/custom-file.el
@@ -0,0 +1,52 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-safe-themes
+   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
+ '(fci-rule-color "#383838")
+ '(lsp-auto-guess-root nil)
+ '(lsp-haskell-server-path "haskell-language-server-wrapper")
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(package-selected-packages
+   '(ormolu reformatter dockerfile-mode terraform-mode js-import zencoding-mode rjsx-mode eslint-fix add-node-modules-path prettier-js ialign region-bindings-mode expand-region multiple-cursors yasnippet-snippets yasnippet yaml-mode lsp-haskell lsp-ivy lsp-treemacs treemacs lsp-ui lsp-mode web-mode web company flycheck tide haskell-mode csv whitespace-cleanup-mode rainbow-delimiters smartparens which-key magit ace-window smex org-bullets ivy-avy counsel zenburn-theme ivy))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(safe-local-variable-values
+   '((eval haskell-stylish-on-save-fn)
+     (eval quote ormolu-format-on-save-mode)
+     (use-ormolu t)))
+ '(standard-indent 2)
+ '(typescript-indent-level 2)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-enable-auto-indentation nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
